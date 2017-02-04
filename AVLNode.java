64afd08895fe90ee830b4f1c9663202c047a6d85
Dillon Heyck <Dillon Heyck>
2017-02-04 14:37:15 -0500

    Fixed files

--- AVLNode.java
+++ AVLNode.java
@@ -1,1 +1,34 @@
-AVLNode
+package songMP3Package;
+
+public class AVLNode
+{
+
+	AVLNode left, right;
+	String name;
+	String cmprName;
+	String artist;
+	String album;
+	String year;
+	int height;
+	
+	public AVLNode()
+	{
+		left = null;
+		right = null;
+		name = null;
+		cmprName = null;
+		artist = null;
+		album = null;
+		year = null;
+		height = 0;
+	}
+	
+	public AVLNode(String name, String artist, String album, String year)
+	{
+		this.name = name;
+		cmprName = (name + artist).toUpperCase();
+		this.artist = artist;
+		this.album = album;
+		this.year = year;
+	}
+}
--- MP3Main.java
+++ MP3Main.java
@@ -1,1 +1,15 @@
-MP3Main
+package songMP3Package;
+
+public class MP3Main {
+
+	public static void main(String args[])
+	{
+		AVLTree tree = new AVLTree();
+		tree.insert("Starboy", "The Weekend", "?", "2015");
+		tree.insert("starboy", "Justin Bieber", "IDK", "2005");
+		tree.insert("Starboy", "Abby Lee", ">>>", "2008");
+		tree.insert("Step It Up", "RuPaul", "LOL", "2");
+		
+		tree.printTree(tree.root);
+	}
+}
